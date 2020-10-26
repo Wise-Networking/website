a6768f370406c919bf16053d75f2aae6c6b25554
Tat Banerjee <tathagat.banerjee@gmail.com>
2020-10-26 16:11:32 +1100

    removed extra section

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -36,31 +36,6 @@ function IndexPage() {
           , a utility-first CSS framework.
         </p>
       </section>
-
-      <section className="text-center">
-        <img
-          alt="Cat and human sitting on a couch"
-          className="block w-1/2 mx-auto mb-8"
-          src={catAndHumanIllustration}
-        />
-
-        <h2 className="inline-block p-3 mb-4 text-2xl font-bold bg-yellow-400">
-          Hey there! Welcome to your first Gatsby site.
-        </h2>
-
-        <p className="leading-loose">
-          This is a barebones starter for Gatsby styled using{` `}
-          <a
-            className="font-bold text-gray-900 no-underline"
-            href="https://tailwindcss.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Tailwind CSS
-          </a>
-          , a utility-first CSS framework.
-        </p>
-      </section>
     </Layout>
   );
 }
