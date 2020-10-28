95565b230332e902324fc059be788dcebe8e92c9
Tat Banerjee <tathagat.banerjee@gmail.com>
2020-10-28 11:37:30 +1100

    not sure about website name yet

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -5,7 +5,7 @@ const fullConfig = resolveConfig(tailwindConfig);
 
 module.exports = {
   siteMetadata: {
-    title: `Dandelion`,
+    title: `Wise Networking`,
     description: `Your Partner For An IOT Future`,
     author: `@taylorbryant`,
   },
