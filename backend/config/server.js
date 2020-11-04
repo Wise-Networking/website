5b108d8b701b0613edb69794451952a44145af32
dipeshwagle <dipeshwagle@icloud.com>
2020-11-04 08:34:51 +0545

    clean master

--- backend/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'f92485f32d8a9f1dffe2e584a3192dac'),
-    },
-  },
-});
--- backend/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'd749aa89-46e8-4df4-b738-cfe1b7305ab2'
-};
