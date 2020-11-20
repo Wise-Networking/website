c452344c142a35152f520e4a2b28aa7b0323bf1b
Tat Banerjee <tathagat.banerjee@gmail.com>
2020-11-20 14:58:01 +1100

    moving blog to news

--- src/templates/news-item.js
+++ src/templates/news-item.js
@@ -1,11 +1,14 @@
 import React from "react"
 import { Link,graphql } from "gatsby"
-import Sidebar from "../components/BlogDetails/Sidebar"
-import Comment from "../components/BlogDetails/Comment"
-import Layout from "../components/App/Layout"
+
 import slugify from 'slugify';
 
-const BlogDetails = props => {
+import Layout from "../components/App/Layout"
+
+import Sidebar from "../components/BlogDetails/Sidebar"
+import Comment from "../components/BlogDetails/Comment"
+
+const NewsItem = props => {
   const post = props.data.datoCmsBlogPost
 
   return (
@@ -67,7 +70,7 @@ const BlogDetails = props => {
 }
 
 export const query = graphql`
-  query getBlogPost($id: String!) {
+  query getNewsItem($id: String!) {
     datoCmsBlogPost(id: { eq: $id }) {
       id
       title
@@ -86,4 +89,4 @@ export const query = graphql`
   }
 `
 
-export default BlogDetails
+export default NewsItem
