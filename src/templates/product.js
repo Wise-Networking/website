fea16a783ce886dab9f5c3779b322b6055320d8c
Tat Banerjee <tathagat.banerjee@gmail.com>
2020-11-24 15:27:12 +1100

    minor styling

--- src/templates/product.js
+++ src/templates/product.js
@@ -1,63 +1,7 @@
 import React from "react"
-import { graphql, Link } from "gatsby"
+import { graphql } from "gatsby"
 import Layout from "../components/App/Layout"
 import BackgroundImage from "gatsby-background-image"
-
-const Product = props => {
-  const product = props.data.datoCmsProduct
-
-  const {
-    title,
-    description,
-    contentNode: {
-      childMarkdownRemark: { html },
-    },
-    featuredImage,
-  } = product
-
-  return (
-    <Layout location="blog">
-      <BackgroundImage
-        Tag="div"
-        className="bread-cumbs-area"
-        fluid={featuredImage.fluid}
-        backgroundColor={`#B68D40`}
-      >
-        <div className="diplay-table">
-          <div className="display-table-cell">
-            <div className="container">
-              <div className="row">
-                <div className="col-lg-7">
-                  <h1>{title}</h1>
-                  <p>{description}</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </BackgroundImage>
-
-      <section id="blog" className="our-blog main-blog bg-none">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-12">
-              <div className="row">
-                <div className="col-lg-12">
-                  <div className="blog-details">
-                    <div
-                      className="post-content"
-                      dangerouslySetInnerHTML={{ __html: html }}
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-    </Layout>
-  )
-}
 
 export const query = graphql`
   query getProduct($id: String!) {
@@ -78,5 +22,61 @@ export const query = graphql`
     }
   }
 `
+
+const Product = props => {
+  const product = props.data.datoCmsProduct
+
+  const {
+    title,
+    description,
+    contentNode: {
+      childMarkdownRemark: { html },
+    },
+    featuredImage,
+  } = product
+
+  return (
+    <Layout location="products">
+      <BackgroundImage
+        Tag="div"
+        className="bread-cumbs-area"
+        fluid={featuredImage.fluid}
+        backgroundColor={`#B68D40`}
+      >
+        <div className="diplay-table">
+          <div className="display-table-cell">
+            <div className="container">
+              <div className="row">
+                <div className="col-lg-7">
+                  <h1>{title}</h1>
+                  <p>{description}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </BackgroundImage>
+
+      <section id="products" className="our-blog main-blog bg-none">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="row">
+                <div className="col-lg-12">
+                  <div className="blog-details">
+                    <div
+                      className="post-content"
+                      dangerouslySetInnerHTML={{ __html: html }}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </Layout>
+  )
+}
 
 export default Product
