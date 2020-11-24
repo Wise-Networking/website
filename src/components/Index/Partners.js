e18f73a5a3da31b405f76c9e9eced6b9e27566c8
Tat Banerjee <tathagat.banerjee@gmail.com>
2020-11-24 11:55:49 +1100

    cleanup products and pricing and delete unused

--- src/components/Index/Partners.js
+++ src/components/Index/Partners.js
@@ -36,6 +36,28 @@ const Partners = () => {
 
   const partners = data.partners.edges.map(node => node.node)
 
+  const partnersData = partners.map((partner, index) => {
+    const {title, link, image} = partner
+    return (
+      <div key={index} className="col-sm-6 col-lg-4">
+        <div className="work-details">
+          <Image fluid={image.fluid}/>
+
+          <div className="box-content">
+            <h3 className="title">{title}</h3>
+            <ul className="icon">
+              <li>
+                <a href={link}>
+                  <i className="fa fa-link"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    )
+  })
+
   return (
     <React.Fragment>
       <section id="partners" className="our-works ptb-100">
@@ -52,33 +74,7 @@ const Partners = () => {
             </div>
           </div>
 
-          <div id="partners" className="row">
-            {
-              partners.map((partner, key) => {
-
-                const {title, link, image} = partner
-
-                return (
-                  <div key={key} className="col-sm-6 col-lg-4">
-                    <div className="work-details">
-                      <Image fluid={image.fluid}/>
-
-                      <div className="box-content">
-                        <h3 className="title">{title}</h3>
-                        <ul className="icon">
-                          <li>
-                            <a href={link}>
-                              <i className="fa fa-link"></i>
-                            </a>
-                          </li>
-                        </ul>
-                      </div>
-                    </div>
-                  </div>
-                )
-              })
-            }
-          </div>
+          <div id="partners" className="row">{partnersData}</div>
         </div>
       </section>
     </React.Fragment>
--- src/templates/product.js
+++ src/templates/product.js
@@ -14,6 +14,7 @@ const Product = props => {
     },
     featuredImage,
   } = product
+
   return (
     <Layout location="blog">
       <BackgroundImage
