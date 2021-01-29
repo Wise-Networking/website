daf2ab7c56417c94978950eea4968d71639036b4
pratapvashi <websitedesignanddeveloper@gmail.com>
2021-01-29 19:43:01 +0530

    worked on 76-website-fixes

--- src/templates/news-post.js
+++ src/templates/news-post.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
 
-import slugify from 'slugify';
+import slugify from "slugify"
 
 import Layout from "../components/App/Layout"
 
@@ -9,14 +9,14 @@ import Sidebar from "../components/NewsDetails/Sidebar"
 
 const NewsItem = props => {
   const post = props.data.datoCmsBlogPost
-
+  console.log("news item ==> ", post)
   var bannerStyle = {
-    backgroundImage: 'url(' + post['featuredImage']?.url + ')'
+    backgroundImage: "url(" + post["featuredImage"]?.url + ")",
   }
 
   return (
     <Layout keywords={post.keywords}>
-      <div className="bread-cumbs-area" style={bannerStyle}/>
+      <div className="bread-cumbs-area" style={bannerStyle} />
 
       <section id="news" className="our-news main-news bg-none">
         <div className="container">
@@ -26,26 +26,40 @@ const NewsItem = props => {
                 <div className="col-lg-12">
                   <div className="news-details">
                     <div className="news-info">
-                      <div className="date-box">{post.publishDay}<span className="month">{post.publishMonth}</span></div>
+                      <div className="date-box">
+                        {post.publishDay}
+                        <span className="month">{post.publishMonth}</span>
+                      </div>
                       <div className="title-meta">
                         <h1>{post.title}</h1>
                         <div className="post-meta">
                           <ul>
-                            <li><i className="fa fa-user"></i><a href="/our-people">{post.author}</a></li>
+                            <li>
+                              <i className="fa fa-user"></i>
+                              <a href="/our-people">{post.author}</a>
+                            </li>
                             <li>
                               <i className="fa fa-tags"></i>
                               {post.tags.map((tag, i) => (
                                 <Link to={`/tags/${slugify(tag.link)}`}>
-                                  { tag.title = i !== post.tags.length - 1 ? tag.title + "," : tag.title}
+                                  {
+                                    (tag.title =
+                                      i !== post.tags.length - 1
+                                        ? tag.title + ","
+                                        : tag.title)
+                                  }
                                 </Link>
                               ))}
                             </li>
-                            <li><i className="fa fa-bars"></i><a href="/categories/">{post.category.title}</a></li>
+                            <li>
+                              <i className="fa fa-bars"></i>
+                              <a href="/categories/">{post.category.title}</a>
+                            </li>
                           </ul>
                         </div>
                       </div>
                     </div>
-                    <br/>
+                    <br />
                     <div
                       className="post-content"
                       dangerouslySetInnerHTML={{
@@ -74,8 +88,8 @@ export const query = graphql`
       slug
       author
       keywords
-      publishDay: publishedDate(formatString:"DD")
-      publishMonth : publishedDate(formatString:"MMM")
+      publishDay: publishedDate(formatString: "DD")
+      publishMonth: publishedDate(formatString: "MMM")
       contentNode {
         childMarkdownRemark {
           html
@@ -86,7 +100,7 @@ export const query = graphql`
         description
         link
       }
-      category{
+      category {
         title
         description
         link
