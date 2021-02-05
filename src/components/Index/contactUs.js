382fe68c90f640524c300da7bc6249c114f186e5
pratapvashi <websitedesignanddeveloper@gmail.com>
2021-02-05 19:06:45 +0530

    worked on formspree form 82 issue

--- src/components/Index/contactUs.js
+++ src/components/Index/contactUs.js
@@ -1,4 +1,4 @@
-import React from "react"
+/*import React from "react"
 import PropTypes from "prop-types"
 import { ValidationForm, TextInput } from "react-bootstrap4-form-validation"
 
@@ -23,10 +23,6 @@ const ContactUs = props => {
           <div className="col-lg-8 offset-lg-2 col-md-8 offset-md-2">
             <div className="contact-form">
 
-
-
-
-              
               <ValidationForm id="contactForm" onSubmit={handleForm}>
                 <div className="row">
                   <div className="col-lg-6 col-md-12">
@@ -123,4 +119,169 @@ ContactUs.defaultProps = {
   Phone: "+61 4 3707 9009",
 }
 
-export default ContactUs
+export default ContactUs*/
+
+
+
+import React from "react";
+// import { ValidationForm, TextInput } from "react-bootstrap4-form-validation";
+export default class ContactUs extends React.Component {
+  constructor(props) {
+    super(props);
+    this.submitForm = this.submitForm.bind(this);
+    this.state = {
+      status: ""
+    };
+  }
+
+  render() {
+    const { status } = this.state;
+    return (
+      // <form
+      //   onSubmit={this.submitForm}
+      //   action="https://formspree.io/f/xknpezrb"
+      //   method="POST"
+      // >
+      //   <label>Email:</label>
+      //   <input type="email" name="email" />
+      //   <label>Message:</label>
+      //   <input type="text" name="message" />
+      //   {status === "SUCCESS" ? <p>Thanks!</p> : <button>Submit</button>}
+      //   {status === "ERROR" && <p>Ooops! There was an error.</p>}
+      // </form>
+
+
+
+
+      <section id="contact-us" className="contact-area">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-8 offset-lg-2 text-center">
+              <div className="section-title">
+                {/* <h2>{props.sectionTitle}</h2>
+              <p>{props.sectionDescription}</p> */}
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-lg-8 offset-lg-2 col-md-8 offset-md-2">
+              <div className="contact-form">
+                <form
+                  onSubmit={this.submitForm}
+                  action="https://formspree.io/f/xknpezrb"
+                  method="POST"
+                >
+                  {/* <ValidationForm id="contactForm" > */}
+                    <div className="row">
+                      <div className="col-lg-6 col-md-12">
+                        <div className="form-group">
+                          {/* <TextInput
+                            name="name"
+                            id="name"
+                            required
+                            successMessage=""
+                            errorMessage="Please enter your name"
+                            className="form-control"
+                            placeholder="Name"
+                            autoComplete="off"
+                          /> */}
+                          <label>Name:</label>
+                          <input type="text" name="name"  className="form-control" autoComplete="off" placeholder="Name" required/>
+                          <div className="help-block with-errors" />
+                        </div>
+                      </div>
+                      <div className="col-lg-6 col-md-12">
+                        <div className="form-group">
+                          {/* <TextInput
+                            name="email"
+                            id="email"
+                            type="email"
+                            required
+                            successMessage=""
+                            errorMessage="Please enter your email address"
+                            className="form-control"
+                            placeholder="Email"
+                            autoComplete="off"
+                          /> */}
+                          <label>Email:</label>
+                          <input type="email" name="email" className="form-control" autoComplete="off" placeholder="Email"  required/>
+                          <div className="help-block with-errors" />
+                        </div>
+                      </div>
+                      <div className="col-lg-12 col-md-12">
+                        <div className="form-group">
+                          {/* <TextInput
+                            name="message"
+                            id="description"
+                            multiline
+                            placeholder="Your message"
+                            className="form-control"
+                            required
+                            successMessage=""
+                            errorMessage="Please write your message"
+                            rows="5"
+                            autoComplete="off"
+                          /> */}
+                          <label>Message:</label>
+                          <input type="text" name="message" className="form-control" autoComplete="off" placeholder="Your message" required/>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="center-wrap text-center">
+                      <div className="button">
+                        {/* <button type="submit">
+                      Submit <i className="fa fa-long-arrow-right"></i>{" "}
+                    </button> */}
+
+                        {status === "SUCCESS" ? <span className="txt-thanks">Thanks!</span> : <button type="submit">
+                          Submit <i className="fa fa-long-arrow-right"></i>{" "}
+                        </button>}
+                        {status === "ERROR" && <p>Ooops! There was an error.</p>}
+
+                        <div className="mask"></div>
+                      </div>
+                    </div>
+                    <div className="clearfix" />
+                  {/* </ValidationForm> */}
+                </form>
+              </div>
+            </div>
+          </div>
+
+
+
+
+
+        </div>
+      </section>
+
+
+
+
+
+
+
+
+    );
+  }
+
+  submitForm(ev) {
+    ev.preventDefault();
+    const form = ev.target;
+    const data = new FormData(form);
+    const xhr = new XMLHttpRequest();
+    xhr.open(form.method, form.action);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        form.reset();
+        this.setState({ status: "SUCCESS" });
+      } else {
+        this.setState({ status: "ERROR" });
+      }
+    };
+    xhr.send(data);
+  }
+}
