3797e25564064cde30c39a2508536cf5bb6b2850
Tat Banerjee <tathagat.banerjee@gmail.com>
2021-02-10 15:05:30 +1100

    mailchimp code

--- gatsby-config.js
+++ gatsby-config.js
@@ -101,7 +101,7 @@ module.exports = {
       resolve: "gatsby-plugin-mailchimp",
       options: {
         endpoint:
-        "https://gmail.us7.list-manage.com/subscribe/post?u=e015e55c8d55076e9d6892fca&amp;id=b67c77420b",
+        "https://work.us4.list-manage.com/subscribe/post?u=12f5efa71026881a906be432f&amp;id=1568b98859",
         timeout: 3500,
       },
     },
