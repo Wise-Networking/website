9a6220427a39108114afd40043ebf2a6c38bc765
Yasin Junet <hello@yasinjunet.com>
2023-12-19 16:49:57 +0700

    style: products page

--- src/pages/products.js
+++ src/pages/products.js
@@ -8,6 +8,7 @@ const getData = graphql`
     products: allDatoCmsProduct {
       edges {
         node {
+          icon
           title
           description
           link
@@ -23,39 +24,56 @@ const Products = props => {
   const productItems = data.products.edges.map(node => node.node)
 
   return (
-    <Layout location="products" keywords="products, wise networking" title="Products | Dandelions offers a number of different solutions for your IOT project. How can we help?" description="Dandelions offers a number of different solutions for your IOT project. How can we help?">
-      <div className="bread-cumbs-area products-banner">
-        <div className="diplay-table"><div className="display-table-cell">
-          <div className="container"><div className="row">
-            <div className="col-lg-7 banner-txt">
+    <Layout
+      location="products"
+      keywords="products, wise networking"
+      title="Products | Dandelions offers a number of different solutions for your IOT project. How can we help?"
+      description="Dandelions offers a number of different solutions for your IOT project. How can we help?"
+    >
+      <section id="news" className="our-news main-news bg-none ptb-100">
+        <div className="container">
+          <div className="row">
+            <div className="col-12 col-md-10 offset-md-1 col-lg-8 offset-lg-2 banner-txt article-header">
               <h1>Products</h1>
-              <p>Dandelions offers a number of different solutions for your IOT project. How can we help?</p>
+              <p>
+                Dandelions offers a number of different solutions for your IOT
+                project. How can we help?
+              </p>
             </div>
           </div>
-          </div>
         </div>
-        </div>
-      </div>
 
-      <section id="news" className="our-news main-news bg-none">
+        <div className="container article-img">
+          <div className="bread-cumbs-area products-banner" />
+        </div>
+
         <div className="container">
           <div className="row products-div">
-            {productItems.map((productItem, key) => {
-              return (
-                <div key={key} className="col-md-6 col-lg-4">
-                  <div className="news-card">
-                    <div className="news-caption">
-                      <h3>{productItem.title}</h3>
-                      <p>{productItem.description}</p>
-
-                      <Link className="read-more" to={`/products/${productItem.link}`}>
-                        Read More
-                          </Link>
+            <div className="col-12 col-md-10 offset-md-1 col-lg-8 offset-lg-2">
+              <div className="row">
+                {productItems.map((productItem, key) => {
+                  return (
+                    <div key={key} className="col-12 col-md-6 pb-4">
+                      <Link
+                        key={key}
+                        className="read-more"
+                        to={`/products/${productItem.link}`}
+                      >
+                        <div className="product-card">
+                          <div className="product-caption">
+                            <div className="text-center">
+                              <i className={productItem.icon}></i>
+                            </div>
+                            <h3>{productItem.title}</h3>
+                            <p>{productItem.description}</p>
+                          </div>
+                        </div>
+                      </Link>
                     </div>
-                  </div>
-                </div>
-              )
-            })}
+                  )
+                })}
+              </div>
+            </div>
           </div>
         </div>
       </section>
